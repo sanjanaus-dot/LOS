2b717cdd9171aab2b9fc69c5a2823e43fceb73d9
sanjanaus-dot <sanjana2002us@gmail.com>
2026-08-11 14:47:22 +0530

    moving file

--- LOS_Lab/tool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter tool name: " tool
-
-if command -v "$tool" &> /dev/null; then
-    echo "$tool is installed."
-else
-    echo "$tool is NOT installed."
-fi
--- LOS_Lab/update.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-sudo apt update
